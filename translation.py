33a82dd7c6844406ad9a9957e6edac3f9830b4a4
AndreaCano <acano@csumb.edu>
2016-04-20 14:51:47 -0700

    Translation so far

---
+++ translation.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from yandex_translate import YandexTranslate
+translate = YandexTranslate('trnsl.1.1.20160420T200137Z.602d58f64d6ac2cf.d8e7eca404d946f979b434e59caa856c178eb095')
+print('Languages: ', translate.langs)
+print('Translate directions: ',translate.directions)
+print('Detect language: ', translate.detect('Привет, мир!'))
+print('Translate: ', translate.translate('Привет, мир!', 'ru-en'))
